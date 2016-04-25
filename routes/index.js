a49cd322ebc4afb9d356b0746f95924e95286cc2
svalient14 <sara.valiente.merino@alumnos.upm.es>
2016-04-25 20:23:15 +0200

    Tema2: Primera Página y Favicon

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
+
+//Un router de express es un MW que permite agrupar otros MWs de atención a rutas
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Quiz' });
 });
 
 module.exports = router;
