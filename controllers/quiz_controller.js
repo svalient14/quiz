0e81c21d2cdee5ee502a3e24d2905225e37003ac
svalient14 <sara.valiente.merino@alumnos.upm.es>
2016-06-01 17:52:33 +0200

    Tema 9: Despliegue DB en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,25 +1,37 @@
 var models = require('../models');
-
-// GET /question
-exports.question = function(req, res, next) {
-	models.Quiz.findOne().then(function(quiz){
-		if(quiz){
-			var answer = req.query.answer || '';
-  			 res.render('quizzes/question', {question: quiz.question,
-			 				answer: answer});
-		}else{ throw new Error('No hay preguntas en la Base de Datos');
-	}).catch(function(error){ next(error);});
-
-};
-
-// GET /check
-exports.check = function(req, res, next) {
-	models.Quiz.findOne().then(function(quiz){
-		if(quiz){
-			var answer = req.query.answer || '';
-			var result = req.query.answer === quiz.answer ? 'Correcta' : 'Incorrecta';
-			res.render('quizzes/result', {result: result,
-						  answer: answer });
-		else{ throw new Error('No hay preguntas en la Base de Datos');}
-	}).catch(function(error){ next(error);});
-};
+ 
+  // GET /question
+  exports.question = function(req, res, next) {
+ 	models.Quiz.findOne() // Busca la primera pregunta existente
+ 		.then(function(quiz) {
+ 			if (quiz) {
+ 				var answer = req.query.answer || '';
+ 				res.render('quizzes/question', {question: quiz.question,
+ 								                answer: answer});
+ 		    } else {
+ 		    	throw new Error('No hay preguntas en la BBDD.');
+ 		    }
+ 		})
+ 		.catch(function(error) {
+ 			next(error);
+ 		});
+  };
+  
+ 
+  // GET /check
+  exports.check = function(req, res, next) {
+ 	models.Quiz.findOne() // Busca la primera pregunta existente
+ 		.then(function(quiz) {
+ 			if (quiz) {
+ 				var answer = req.query.answer || "";
+ 				var result = answer === quiz.answer ? 'Correcta' : 'Incorrecta';
+ 				res.render('quizzes/result', { result: result, 
+ 											   answer: answer });
+ 		    } else {
+ 		    	throw new Error('No hay preguntas en la BBDD.');
+ 		    }
+ 		})
+ 		.catch(function(error) {
+ 			next(error);
+ 		});
+  };
---
+++ models/index.js
@@ -0,0 +1,50 @@
+
+ var path = require('path');
+ 
+ // Cargar Modelo ORM
+ var Sequelize = require('sequelize');
+ 
+ // Usar BBDD SQLite:
+// DATABASE_URL = postgres://user:passwd@host:port/database
+var url,storage;
+
+if(!process.env.DATABASE_URL){
+	url="sqlite:///";
+	storage="quiz.sqlite";
+} else{
+	url=process.env.DATABASE_URL;
+	storage=process.env.DATABASE_STORAGE||"";
+}
+
+
+ var sequelize = new Sequelize(url,{ storage: storage, 
+                        	   omitNull: true
+                        		  });
+ 
+ // Importar la definicion de la tabla Quiz de quiz.js
+ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+ 
+ 
+ // sequelize.sync() crea e inicializa tabla de preguntas en DB
+ sequelize.sync()
+     .then(function() {
+         // Ya se han creado las tablas necesarias.
+         return Quiz.count()
+                 .then(function (c) {
+                     if (c === 0) {   // la tabla se inicializa solo si está vacía
+                         return Quiz.create({ question: 'Capital de Italia',
+           	                                 answer: 'Roma'
+           	                               })
+                                    .then(function() {
+                                         console.log('Base de datos inicializada con datos');
+                                     });
+                     }
+                 });
+     })
+     .catch(function(error) {
+         console.log("Error Sincronizando las tablas de la BBDD:", error);
+         process.exit(1);
+     });
+ 
+ 
+ exports.Quiz = Quiz; // exportar definición de tabla Quiz
